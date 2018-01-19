6be54ca1ed414bec0b320ca4d89d4c99b4d66ba9
sunqingbiao <1196239739@qq.com>
2018-01-19 17:31:01 +0800

    0026提交

---
+++ 0026/idCard.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+' 第 0026 题： 使用python开发一个身份证号在线生成工具 '
+
+__author__ = 'sunqb'
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+print 'ok'
